f0e726b3a1eee844dd866da9458795364fdf6384
Anandya Almas <byanmalang123@gmail.com>
2024-10-11 07:17:30 +0700

    modifikasi siakadfor

--- SiakadFor03.java
+++ SiakadFor03.java
@@ -5,6 +5,8 @@ public class SiakadFor03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0;
+        int jumlahTidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,8 +17,21 @@ public class SiakadFor03 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                jumlahLulus++;
+            } else {
+                jumlahTidakLulus++;
+            
+                
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + jumlahLulus);
+        System.out.println("jumlah mahasiswa tidak lulus " + jumlahTidakLulus);
+
+        sc.close();
     }
 }
+
